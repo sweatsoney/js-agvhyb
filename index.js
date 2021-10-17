4d73f623ef5294c2dbbf67ee8a17e5fea65bdd38
sweatsoney <skxpped087@gmail.com>
2021-10-17 14:46:37 -0400

    3 files modified

--- index.js
+++ index.js
@@ -3,4 +3,6 @@ import './style.css';
 
 // Write Javascript code!https://github.com/wwwg/UltraTypeBot.git
 const appDiv = document.getElementById('app');
-appDiv.innerHTML = `<h1>JS Starter</h1>`;
+appDiv.innerHTML = `<h1>JS Starter</h1>`;res;
+if (href == "https://www.nitrotype.com/race") res = true;
+"https://www.nitrotype.com/race/")
